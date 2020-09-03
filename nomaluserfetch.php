4b4eb54c94f10afd8fb352dc6fd076a1fc9e3ced
LeeHyunseok <rapael3020@gmail.com>
2020-09-03 22:04:06 +0900

    유저정보가져오기

---
+++ nomaluserfetch.php
@@ -0,0 +1,16 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$password = '111111';
+$database_name = 'LoadingArm';
+ 
+$conn = new mysqli($host, $user, $password, $database_name);
+$query = "select n_id, n_name, n_num, n_phone, n_addr, n_pw from nomal_user";
+
+$result = mysqli_query($conn, $query);
+
+while($data = mysqli_fetch_row($result)){
+   print_r($data);
+   echo '<br>';
+}
+?>
