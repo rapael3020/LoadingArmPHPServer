850e20130c2f4007791b2629c280799c1bd6e741
LeeHyunseok <rapael3020@gmail.com>
2020-09-17 23:08:37 +0900

    1

---
+++ example.php
@@ -0,0 +1,13 @@
+<?php
+ session_start();
+ if (isset($_SESSION["test"]))
+ {
+     $_SESSION["test"]++;
+     echo "welcome again " . $_SESSION["test"];
+ }
+ else
+ {
+     $_SESSION["test"] = 0;
+     echo "welcome";
+ }
+ ?> 
