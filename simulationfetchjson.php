30a96912a713cf4d499c051a8f3cb21cb7e9a5df
LeeHyunseok <rapael3020@gmail.com>
2020-09-18 11:46:06 +0900

    json data  php

---
+++ simulationfetchjson.php
@@ -0,0 +1,23 @@
+<?php
+$host = '';
+$user = 'test';
+$password = '';
+$database_name = 'LoadingArm';
+$host = '3306';
+ 
+$con = new mysqli($host, $user, $password, $database_name, $host);
+if (mysqli_connect_errno($con))
+ { echo "MySQL접속 실패: " . mysqli_connect_error(); }
+
+
+ mysqli_set_charset($con,"utf8");
+ $res = mysqli_query($con, "select*from simulation");
+ $result = array();
+ while ($row = mysqli_fetch_array($res)) {
+    array_push($result, array('s_x1'=>$row[0],'s_y1'=>$row[1],'s_z1'=>$row[2]));
+ }
+ echo json_encode(array("result"=>$result),JSON_UNESCAPED_UNICODE);
+
+ mysqli_close($con);
+ ?>
+
